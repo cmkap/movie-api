be1f4eec08ff3dec96a5e901a7bdca55bdd1ef93
chisambwe.k <chisambwe.k@carbonyte.io>
2023-08-29 17:21:16 +0100

    Refactor two phase commit to Transactions

--- routes/rentals.js
+++ routes/rentals.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 const { Rental, schema } = require("../model/rentals");
 const { Customer } = require("../model/customer");
 const { Movie } = require("../model/movies");
-const router = express.Router();
 
 router.get("/", async (req, res) => {
   const rentals = await Rental.find().sort("-dateOut");
@@ -13,13 +14,15 @@ router.post("/", async (req, res) => {
   const { value, error } = schema.validate(req.body);
   if (error) return res.status(400).send(error.message);
 
-  const customer = await Customer.findById(value.customerID);
+  console.log(value);
+  const customer = await Customer.findById(value.customerId);
   if (!customer) return res.status(400).send("Invalid customer.");
 
-  const movie = await Movie.findById(value.movieID);
+  const movie = await Movie.findById(value.movieId);
   if (!movie) return res.status(400).send("Invalid movie.");
 
-  if (movie.numberInStock === 0 ) return res.status(400).send('Movie not in stock')
+  if (movie.numberInStock === 0)
+    return res.status(400).send("Movie not in stock");
 
   let rental = new Rental({
     customer: {
@@ -34,14 +37,22 @@ router.post("/", async (req, res) => {
     },
   });
 
-  rental = await rental.save()
-
-  movie.numberInStock--;
-  await movie.save();
-  
-  res.send(rental)
 
 
+  try {
+    const session = await mongoose.startSession();
+    await session.withTransaction(async () => {
+      const result = await rental.save();
+      movie.numberInStock--;
+      movie.save();
+      res.send(result);
+    });
+
+    session.endSession();
+    console.log("success");
+  } catch (error) {
+    console.log("error111", error.message);
+  }
 });
 
 module.exports = router;
