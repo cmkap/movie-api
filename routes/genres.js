43842e1b549fa95aa3b27e89d5d18dedc9907a02
chisambwe.k <chisambwe.k@carbonyte.io>
2023-08-30 18:08:07 +0100

    Refactor trycatch

--- routes/genres.js
+++ routes/genres.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const { Genre, schema } = require("../model/genre");
+const asyncMiddleware = require("..//middleware/async")
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 
-router.get("/", (req, res, next) => {
-  Genre.find()
-    .sort("name")
-    .select("name")
-    .then((genres) => res.status(200).send(genres))
-    .catch((err) => next(err));
-});
+
+
+router.get("/", asyncMiddleware(async (req, res) => {
+    const genres = await Genre.find().sort("name").select("name");
+    res.status(200).send(genres);
+  })
+);
 
 router.post("/", auth, (req, res, next) => {
   const { value, error } = schema.validate(req.body);
@@ -25,14 +26,14 @@ router.post("/", auth, (req, res, next) => {
     .catch((err) => next(err));
 });
 
-router.put("/:id", auth, async (req, res, next) => {
+router.put("/:id", auth, asyncMiddleware(async (req, res, next) => {
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).send(error.message);
 
   const _id = req.params.id;
   const genreUpdate = req.body.name;
 
-  try {
+  
     const genre = await Genre.findByIdAndUpdate(
       _id,
       { name: genreUpdate },
@@ -41,35 +42,29 @@ router.put("/:id", auth, async (req, res, next) => {
     if (!genre) return res.status(404).send("Genre not found");
 
     return res.status(200).send(genre);
-  } catch (error) {
-    next(error);
-  }
-});
+  
+}));
 
 router.delete("/:id", [auth, admin], async (req, res, next) => {
   const _id = req.params.id;
 
-  try {
+
     const genre = await Genre.findByIdAndRemove(_id);
     if (!genre) return res.status(404).send("Genre not found");
 
     return res.status(200).send(genre);
-  } catch (error) {
-    next(error);
-  }
+  
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", asyncMiddleware(async (req, res, next) => {
   const _id = req.params.id;
 
-  try {
+ 
     const genre = await Genre.findById(_id);
     if (!genre)
       return res.status(404).send("The genre with the given ID was not found.");
     return res.status(200).send(genre);
-  } catch (error) {
-    next(error);
-  }
-});
+  
+}));
 
 module.exports = router;
