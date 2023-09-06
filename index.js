eabf4d490d768b2607637f695c3a5ffdfac11146
chisambwe.k <chisambwe.k@carbonyte.io>
2023-09-06 17:19:56 +0100

    Handle unhandled promise rejections

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
-require('dotenv').config()
-require('express-async-errors')
-const winston = require('winston')
-const Joi = require('joi')
-Joi.objectId = require('joi-objectid')(Joi)
-const mongoose = require('mongoose')
+require("dotenv").config();
+require("express-async-errors");
+const winston = require("winston");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
 const debug = require("debug")("app:startup");
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const app = express();
 
-process.on('uncaughtException', (ex) => {
-  console.log('WE GOT AN UNCAUGHT EXCEPTION')
-  winston.error(ex.message, ex)
-})
-
-winston.add(new winston.transports.File({filename:'logfile.log'}))
+process.on("uncaughtException", (ex) => {
+  winston.error(ex.message, ex);
+  process.exit(1);
+});
 
 
+process.on("uncaughtRejection", (ex) => {
+  winston.error(ex.message, ex);
+  process.exit(1);
+});
 
-const logger = require('./middleware/logger')
+winston.add(new winston.transports.File({ filename: "logfile.log" }));
+
+const logger = require("./middleware/logger");
 const hompage = require("./routes/home");
 const genres = require("./routes/genres");
-const customers = require('./routes/customers')
-const movies = require("./routes/movies") 
-const rentals = require("./routes/rentals")
-const users = require("./routes/users")
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
 const auth = require("./routes/auth");
-const error = require('./middleware/error');
+const error = require("./middleware/error");
 
-if(!process.env.JWT_PRIVATE_KEY) {
-  console.log('FATAL ERROR: jwt private key is not defined')
-  process.exit(1)
+if (!process.env.JWT_PRIVATE_KEY) {
+  console.log("FATAL ERROR: jwt private key is not defined");
+  process.exit(1);
 }
-const username = process.env.USERNAME
-const password = process.env.SECRET_KEY
+const username = process.env.USERNAME;
+const password = process.env.SECRET_KEY;
 
-const uri = `mongodb+srv://${username}:${password}@cluster0.xe4jeek.mongodb.net/movie?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${username}:${password}@cluster0.xe4jeek.mongodb.net/movie?retryWrites=true&w=majority`;
 
-mongoose.connect(uri)
-  .then(debug('Connected to MongoDB'))
-  .catch((error) => console.error('Error: ', error))
-
-
+mongoose
+  .connect(uri)
+  .then(debug("Connected to MongoDB"))
+  .catch((error) => console.error("Error: ", error));
 
 app.set("view engine", "pug");
 
@@ -63,13 +66,13 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use('/', hompage)
+app.use("/", hompage);
 app.use("/api/genres", genres);
-app.use('/api/customers', customers)
-app.use('/api/movies', movies)
-app.use('/api/rentals', rentals)
-app.use('/api/users', users)
-app.use('/api/auth', auth)
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 app.use(error);
 
