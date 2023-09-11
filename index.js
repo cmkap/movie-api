d88c3224597e0d916340083c4b5a2d574e71b700
chisambwe.k <chisambwe.k@carbonyte.io>
2023-09-11 17:30:07 +0100

    Extract Logging logic

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 require("dotenv").config();
-require("express-async-errors");
-const winston = require("winston");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const debug = require("debug")("app:startup");
 const express = require("express");
-
 const app = express();
+
+require('./startup/logging')()
 require('./startup/routes')(app, debug)
 require('./startup/db')()
 
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-process.on("uncaughtRejection", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
 
 if (!process.env.JWT_PRIVATE_KEY) {
   console.log("FATAL ERROR: jwt private key is not defined");
