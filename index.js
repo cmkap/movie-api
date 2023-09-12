68c3d6935d1a4eeb1df659af165086b09af83b70
chisambwe.k <chisambwe.k@carbonyte.io>
2023-09-12 17:14:07 +0100

    Extract validation and Update startup order

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
+const debug = require("debug")("app:startup");
 const express = require("express");
 const app = express();
 
 require('./startup/logging')()
-require('./startup/routes')(app)
-require('./startup/db')()
-require('./startup/config')()
 require('./startup/validation')()
+require('./startup/routes')(app, debug)
+require('./startup/db')()
+require('./startup/config')
 
+// app.set("view engine", "pug");
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
