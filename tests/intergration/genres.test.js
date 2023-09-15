e1c19d0396c84433229a33181cfd4f72978a7f7e
chisambwe.k <chisambwe.k@carbonyte.io>
2023-09-15 17:06:04 +0100

    Add Test for genres GET /:id

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,5 +1,7 @@
 const request = require("supertest");
-const { Genre } =require("../../models/genre");
+const { Genre } = require("../../models/genre");
+const mongoose = require('mongoose')
+
 let server;
 
 describe("/api/genres", () => {
@@ -8,23 +10,39 @@ describe("/api/genres", () => {
   });
   afterEach(async () => {
     server.close();
-    await Genre.deleteMany({})
+    await Genre.deleteMany({});
   });
 
   describe("GET /", () => {
     it("should return all genres", async () => {
-        await Genre.collection.insertMany([
-            { name: 'genre1'},
-            { name: 'genre2'}
-        ])
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
 
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
-      expect(res.body.length).toBe(2)
-      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy()
-      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy()
-
-      
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
     });
   });
+
+  describe("GET /:id", () => {
+    it("should return a genre if valid id is passed", async() => {
+        // direct into db
+        // await Genre.collection.insertOne({
+        //     name: "genre1"
+        // })
+
+        // useses mongoose, applies validation, miiddleware and schema definitions -- access to _id
+        const genre = new Genre({name: 'Drama'})
+        await genre.save()
+
+        const  res = await request(server).get('/api/genres/' + genre._id)
+
+        expect(res.status).toBe(200)
+        expect(res.body).toHaveProperty('name', genre.name)
+    })
+  })
 });
